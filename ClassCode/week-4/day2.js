fa6a68ea4b0a62c9e562dbfaac045569bcce1ed6
Prakash <urprakashgupta@gmail.com>
2026-02-09 20:44:18 +0530

    Week : 4

---
+++ ClassCode/week-4/day2.js
@@ -0,0 +1,14 @@
+// console.log(age);
+
+// let age = 45;
+// console.log(age);
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result; // return is the last statement of a function
+}
+
+const result = add(4, 4);
+for (let i = 0; i <= result; i++) {
+  console.log(`The value of result is ${i}`);
+}
---
+++ ClassCode/week-4/script.js
@@ -0,0 +1,22 @@
+// var age = 10;
+// var childCondition = age <= 12;
+// var teenCondition = age <= 19;
+// var adultCondition = age <= 25;
+// var seniorCondition = age > 31;
+
+// if (childCondition) {
+//     console.log('You are a child');
+// } else if (teenCondition) {
+//     console.log('You are a teen');
+// } else if (adultCondition) {
+//     console.log('You are an Adult');
+// } else if (seniorCondition) {
+//     console.log('You are an Senior Citizen');
+// } else {
+//     console.log('Sabhi condition false ho gye !!');
+
+// }
+
+
+// ! loop 
+
