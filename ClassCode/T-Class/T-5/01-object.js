02754593f02f8af36251539a4959f8165bb8244c
Prakash <urprakashgupta@gmail.com>
2026-02-27 23:13:43 +0530

    feat: add initial object and greeting function examples

---
+++ ClassCode/T-Class/T-5/01-object.js
@@ -0,0 +1,19 @@
+let details = {
+    name: "Prakash",
+    age: 45,
+    town: "jodhpur"
+}
+console.log(details);
+
+
+let codes = {
+    7: "Canada",
+    91: "india",
+    98: "pakasistan"
+}
+for (const code in codes) {
+
+    console.log(codes);
+}
+
+
