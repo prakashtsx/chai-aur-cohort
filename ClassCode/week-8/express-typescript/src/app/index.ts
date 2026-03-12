7e4d659b0065505e35a58186b9a05153c9c654de
Prakash <urprakashgupta@gmail.com>
2026-03-12 21:12:38 +0530

    refactor: clean up index.ts formatting and add a new dom.js file

--- ClassCode/week-8/express-typescript/src/app/index.ts
+++ ClassCode/week-8/express-typescript/src/app/index.ts
@@ -1,14 +1,13 @@
-import express from 'express'
-import type { Application } from 'express'
-import todoRouter from './todo/routes.js'
-
+import express from "express";
+import type { Application } from "express";
+import todoRouter from "./todo/routes.js";
 
 export function createServerApplication(): Application {
-    const app = express()
+  const app = express();
 
-    app.use(express.json())
+  app.use(express.json());
 
-    app.use('/todos', todoRouter)
+  app.use("/todos", todoRouter);
 
-    return app
+  return app;
 }
