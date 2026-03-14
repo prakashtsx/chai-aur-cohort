1c39833cb2674367ecd93919952eff06204480d2
Prakash <urprakashgupta@gmail.com>
2026-03-14 20:16:53 +0530

    feat: add color plate generator with HTML, CSS, and JavaScript files

---
+++ ClassCode/T-Class/T-7/color-plate-generator/main.js
@@ -0,0 +1,10 @@
+const formatSelect = document.getElementById("format");
+const toneSelect = document.getElementById("tone");
+const button = document.getElementById("generateBtn");
+
+function generatePalette() {
+    console.log(formatSelect.value);
+    console.log(toneSelect.value);
+
+
+}
